2e0d2037393d01f73a85d8c178f6e471477a6a9a
Alex Le <thanhdungvn@gmail.com>
2022-12-07 10:04:17 +0700

    test ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
 
 // TODO: Please implement this component.
 function App() {
+  const [tasks, setTasks] = React.useState<string[]>([
+    'Learn React',
+    'Learn TypeScript',
+  ])
+  const [newTask, setNewTask] = React.useState<string>('')
+
   return (
-    <div className='App'>
-      <h2>TODO App</h2>
+    <div className="App">
+      <Header />
+      <div
+        style={{
+          border: '1px solid red',
+          width: '1000px',
+          margin: 'auto',
+          textAlign: 'left',
+          padding: '10px',
+        }}
+      >
+        <p>Task List</p>
+
+        <ol
+          style={{
+            listStyleType: 'none',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            gap: '10px',
+          }}
+        >
+          {tasks.map((task, index) => (
+            <div
+              key={index}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+              }}
+            >
+              <li key={'todo-item' + index.toString()}>{task}</li>
+              <button
+                onClick={() => {
+                  const newTasks = [...tasks]
+                  newTasks.splice(index, 1)
+                  setTasks(newTasks)
+                }}
+              >
+                Done
+              </button>
+            </div>
+          ))}
+        </ol>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+const Header = () => <div>
+    <h1>Header</h1>
+</div>
+
+export default Header
